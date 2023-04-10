3962475ec23f5e77799fcac5faceb2d802a66a8a
Michael <mcadieux0730@gmail.com>
2023-04-09 23:45:46 -0500

    nav and foot

---
+++ js/foot.js
@@ -0,0 +1,6 @@
+document.write('\
+    <div class="footer">\
+        <a href="https://www.instagram.com/cadieuxbadger/" target="_blank"><img class="link-icon" src="images/insta.png"></a>\
+        <a href="https://github.com/cadieuxbadger" target="_blank"><img class="link-icon" src="images/git.png"></a>\
+    </div>\
+');
---
+++ js/nav.js
@@ -0,0 +1,20 @@
+document.write('\
+    <nav class="navbar navbar-dark navbar-expand-lg my-nav">\
+        <div class="container-fluid">\
+            <a class="navbar-brand" href="index.html">Navbar</a>\
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">\
+                    <li class="nav-item">\
+                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a class="nav-link" href="#">Link</a>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a class="nav-link">Disabled</a>\
+                    </li>\
+                </ul>\
+            </div>\
+        </div>\
+    </nav>\
+');
