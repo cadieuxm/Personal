98878c5a6b9c1f067452c854d87b85c5e5b5060f
Michael <mcadieux0730@gmail.com>
2023-04-10 00:07:12 -0500

    color and new pages

--- js/foot.js
+++ js/foot.js
@@ -1,5 +1,6 @@
 document.write('\
     <div class="footer">\
+        <div class="foot-text">&copy; 2023 Michael Cadieux</div>\
         <a href="https://www.instagram.com/cadieuxbadger/" target="_blank"><img class="link-icon" src="images/insta.png"></a>\
         <a href="https://github.com/cadieuxbadger" target="_blank"><img class="link-icon" src="images/git.png"></a>\
     </div>\
--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,17 @@
 document.write('\
     <nav class="navbar navbar-dark navbar-expand-lg my-nav">\
         <div class="container-fluid">\
-            <a class="navbar-brand" href="index.html">Navbar</a>\
+            <a class="navbar-brand" href="index.html"><h1>Michael Cadieux</h1></a>\
             <div class="collapse navbar-collapse" id="navbarSupportedContent">\
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">\
                     <li class="nav-item">\
-                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>\
+                        <a class="nav-link" aria-current="page" href="index.html">Home</a>\
                     </li>\
                     <li class="nav-item">\
-                        <a class="nav-link" href="#">Link</a>\
+                        <a class="nav-link" href="art.html">Art</a>\
                     </li>\
                     <li class="nav-item">\
-                        <a class="nav-link">Disabled</a>\
+                        <a class="nav-link" href="code.html">Code</a>\
                     </li>\
                 </ul>\
             </div>\
