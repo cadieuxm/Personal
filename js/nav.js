ed10cf97f1462d41d0943a224ab5e3a0afdd713c
Michael <mcadieux0730@gmail.com>
2023-11-26 19:04:54 -0600

    added images and list to work section

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,9 @@
 document.write('\
     <nav class="navbar navbar-expand-lg my-nav">\
         <div class="container-fluid">\
-            <a class="navbar-brand" href="index.html"><img class = "nav-img" src = "assets/m.png" ></a>\
+            <a class="navbar-brand" href="#intro"><img class = "nav-img" src = "assets/m.png" ></a>\
             <div class="collapse navbar-collapse" id="navbarSupportedContent">\
                 <ul class="navbar-nav me-auto ms-lg-4">\
-                    <li class="nav-item">\
-                        <a class="nav-link" aria-current="page" href="index.html">Home</a>\
-                    </li>\
                     <li class="nav-item">\
                         <a class="nav-link" href="#about">About Me</a>\
                     </li>\
