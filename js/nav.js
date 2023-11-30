955478714d36af849c610d3269ef8d7c415a6cde
Michael <mcadieux0730@gmail.com>
2023-11-29 20:06:23 -0600

    fix navbar styling

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,18 @@
 document.write('\
     <nav class="navbar navbar-expand-lg my-nav">\
-        <div class="container-fluid">\
-            <a class="navbar-brand" href="#intro"><img class = "nav-img" src = "assets/M.png" ></a>\
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">\
-                <ul class="navbar-nav me-auto ms-lg-4">\
-                    <li class="nav-item">\
-                        <a class="nav-link" href="#about">About Me</a>\
-                    </li>\
-                    <li class="nav-item">\
-                        <a class="nav-link" href="#work">My Work</a>\
-                    </li>\
-                </ul>\
-            </div>\
+        <a class="navbar-brand" href="#intro"><img class = "nav-img" src = "assets/M.png" ></a>\
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\
+          <span class="navbar-toggler-icon"></span>\
+        </button>\
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">\
+            <ul class="navbar-nav mr-auto">\
+                <li class="nav-item">\
+                    <a class="nav-link" href="#about">About Me</a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link" href="#work">My Work</a>\
+                </li>\
+            </ul>\
         </div>\
     </nav>\
 ');
